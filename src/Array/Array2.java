a77f230e6e46bf58f7eede9e75f4b16bea4bf806
Lazuardi13 <62470757+Lazuardi13@users.noreply.github.com>
2020-04-25 23:23:26 +0800

    Array2

---
+++ src/Array/Array2.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class Array2 {
+    public static void main(String[] args) {
+        String mataKuliah[];
+        mataKuliah = new String [10];
+
+        mataKuliah[0] = "Pemrograman Berbasis Objek 1";
+        mataKuliah[1] = "Viual 2";
+        mataKuliah[2] = "Rekayasa Perangkat Lunak";
+        mataKuliah[3] = "Organisasi Komputer";
+        mataKuliah[4] = "Pemrograman Web2";
+    }
+}
